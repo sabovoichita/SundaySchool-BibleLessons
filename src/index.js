c9e02392ebc114dcf011955b2a73396b1338a49b
Voichita Maria <sabo_voichita@yahoo.com>
2024-03-26 11:22:09 +0100

    Cross Out- example1

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./style.css";
+document.querySelector("#div1").addEventListener("click", e => {
+  e.target.classList.add("crossClick");
+});
+
+document.querySelector("#div2").addEventListener("click", e => {
+  e.target.classList.add("crossClick");
+});
+document.querySelector("#div3").addEventListener("click", e => {
+  e.target.classList.add("crossClick");
+});
+document.querySelector("#div4").addEventListener("click", e => {
+  e.target.classList.add("crossClick");
+});
+document.querySelector("#div5").addEventListener("click", e => {
+  e.target.classList.add("crossClick");
+});
